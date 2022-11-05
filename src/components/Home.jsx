0cf38ca9a5895ceefcb088cf2dd1bf6f8c580213
daniel-valean <dannyvalean@gmail.com>
2022-11-05 15:45:15 -0700

    home page and view button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,18 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+
+        {/* container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <h1 className='text-4xl sm:text-7xl font-bold text-white'>Daniel Valean</h1>
+        
+            <div>
+                <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-gray'>View Work<HiArrowNarrowRight /></button>
+            </div>
+
+        </div>
+
 
     </div>
   )
