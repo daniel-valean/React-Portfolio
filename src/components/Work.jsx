08c5a0f8b27bc82b83d5f4bfd51189450b00205a
daniel-valean <dannyvalean@gmail.com>
2022-11-07 00:25:18 -0700

    work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-white border-gray-600'>Work</p>
+                <p className='py-6'>Check out my work.</p>
+            </div>
+        </div>
+
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            <div className='shadow-lg shadow-black group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                {/* hover effects */}
+                <div>
+                    <span>
+
+                    </span>
+                    <div>
+                        <a href="">
+                            <button></button>
+                        </a>
+                        <a href="">
+                            <button></button>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
