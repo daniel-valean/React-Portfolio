77aa24ce0a544ce41fdeb04b936877190c22c3f5
daniel-valean <dannyvalean@gmail.com>
2022-11-07 00:03:07 -0700

    skills page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,19 @@ const About = () => {
         <div className='flex flex-col justify-center items-center w-full h-full'>
             <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
                 <div className='sm:text-right pb-8 pl-4'>
-                    <p className='text-4xl font-bold inline border-b-4'>About</p>
+                    <p className='text-5xl font-bold inline border-b-4 border-gray-600'>About</p>
+                    <div></div>
                 </div>
             </div>
-            <div>
-
+              <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                <div className='sm:text-right text-5xl font-bold'>
+                  <p>Hi I'm Daniel, nice to meet you.</p>
+                </div>
+                <div className='text-2xl'>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, corporis 
+                  maiores officiis dolorum quas omnis non harum pariatur et aliquam sunt repellendus 
+                  nulla quae modi, ullam blanditiis cupiditate rerum nihil.</p>
+                </div>
             </div>
         </div>
     </div>
