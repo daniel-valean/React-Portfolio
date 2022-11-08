17d4567294905ae04ea6411f3ccdf2ab6f59062b
daniel-valean <dannyvalean@gmail.com>
2022-11-07 23:35:34 -0700

    smooth scroll

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify center items-center p-4'>
-            <form action="" className='flex flex-col max-w-[600px] w-full'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+            <form method='POST' action="https://getform.io/f/df14e171-a498-41f8-8d20-96162deb1c43" className='flex flex-col max-w-[600px] w-full'>
                 <div className='pb-8'>
-                    <p className='text-4xl front-bold inline border-b-4 text-white border-gray-600'>Contact</p>
+                    <p className='text-5xl front-bold inline border-b-4 text-white border-gray-600'>Contact</p>
                     <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - Dannyvalean@gmail.com</p>
                 </div>
-                <input className='text-white p-2' type="text" placeholder='Name' name='name'/>
+                <input className='bg-white p-2' type="text" placeholder='Name' name='name'/>
                 <input className='my-4 p-2 bg-white' type="email" placeholder='Email' name='email'/>
                 <textarea className='bg-white p-2' name="message" rows="10" placeholder='Message'></textarea>
+                <button className='text-white border-2 hover:border-gray-600 hover:bg-gray-600 px-4 py-3 my-8 mx-auto flex items-center'>Send</button>
             </form>
     </div>
   )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/f
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../assets/dvlogo.png';
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -16,11 +17,31 @@ return (
 
         {/* menu */}
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                <Link to="home" smooth={true} offset={50} duration={500}>
+                Home
+                </Link>
+                </li>
+                <li>                
+                <Link to="about" smooth={true} offset={50} duration={500}>
+                About
+                </Link>
+                </li>
+                <li>
+                <Link to="skills" smooth={true} offset={50} duration={500}>
+                Skills
+                </Link>
+                </li>
+                <li>
+                <Link to="work" smooth={true} offset={50} duration={500}>
+                Work
+                </Link>
+                </li>
+                <li>
+                <Link to="contact" smooth={true} offset={50} duration={500}>
+                Contact
+                </Link>
+                </li>
             </ul>
 
             {/* hamburger */}
