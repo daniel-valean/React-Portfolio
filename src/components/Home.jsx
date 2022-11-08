156316c9e36a987f5264c1d787a8f862c1843e35
daniel-valean <dannyvalean@gmail.com>
2022-11-08 01:36:34 -0700

    view workout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import {Link} from 'react-scroll'
+
 
 const Home = () => {
   return (
@@ -7,15 +8,14 @@ const Home = () => {
 
         {/* container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <h1 className='text-4xl sm:text-7xl font-bold text-white'>Daniel Valean</h1>
+            <h1 className='text-4xl sm:text-7xl font-bold text-white'>
+              Daniel Valean</h1>
         
             <div>
-                <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-gray-600 hover: border-gray-600'>View Work<HiArrowNarrowRight /></button>
+                <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-gray-600 hover: border-gray-600'>
+                <Link to="work" smooth={true} offset={50} duration={500}>View Work</Link></button>
             </div>
-
         </div>
-
-
     </div>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -51,11 +51,31 @@ return (
 
             {/* mobile */}
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Skills</li>
-                <li className='py-6 text-4xl'>Work</li>
-                <li className='py-6 text-4xl'>Contact</li>
+                <li className='py-6 text-4xl'>           
+                <Link onClick={handleClick} to="home" smooth={true} offset={50} duration={500}>
+                Home
+                </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="about" smooth={true} offset={50} duration={500}>
+                About
+                </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="skills" smooth={true} offset={50} duration={500}>
+                Skills
+                </Link>
+                </li>
+                <li onClick={handleClick} className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="work" smooth={true} offset={50} duration={500}>
+                Work
+                </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="contact" smooth={true} offset={50} duration={500}>
+                Contact
+                </Link>
+                </li>
             </ul>
 
             {/* social */}
